8b36e0ae01ee2d80531aa48ef27c273b03a4d555
Marlisson Silva <marlisson.business@gmail.com>
2023-03-23 11:55:47 -0300

    fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window_width: int = 360
+    #chimeracam_window: MainWindow = MainWindow()
     chimeracam_window: ChimeraCam = ChimeraCam(window_width)
     chimeracam_window.show()
     sys.exit(app.exec())
