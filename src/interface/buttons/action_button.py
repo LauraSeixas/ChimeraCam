b5547808a17c6c7a0e6cbd18a2a3a7447bdd2fce
Marlisson Silva <marlisson.business@gmail.com>
2023-03-23 12:43:08 -0300

    refact

--- src/interface/buttons/action_button.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QPushButton
-
-class ActionButton(QPushButton):
-    def __init__(self, button_text: str):
-        super(ActionButton, self).__init__()
-        
-        self.setText(button_text)
-        self.setStyleSheet("background-color: #3F95D3; border-radius: 10px; border: 1px solid #FFFFFF")
--- src/interface/buttons/alarm_button.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QPushButton
-
-class AlarmButton(QPushButton):
-    def __init__(self, button_text: str):
-        super(AlarmButton, self).__init__()
-        
-        self.setText(button_text)
-        self.setStyleSheet("background-color: #D33F3F; border-radius: 10px; border: 1px solid #FFFFFF")
