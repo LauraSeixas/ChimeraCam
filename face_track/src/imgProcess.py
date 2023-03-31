4fda99004b56c8f78c386df8b121a84594e84652
Luan <luan.mesquita.sobrinho@gmail.com>
2023-03-31 11:32:02 -0300

    resizing images

--- face_track/src/imgProcess.py
+++ face_track/src/imgProcess.py
@@ -58,6 +58,12 @@ class FaceTrack:
                     
                     id_ = label_ids[label]
                     img = cv2.imread(path)
+                    target_width = 550
+                    height, width, _ = img.shape
+                    ratio = width / height
+
+                    target_height = int(target_width / ratio)
+                    img = cv2.resize(img, (target_width, target_height))
 
                     ###################################################################
                     #### here is using pill, and recising, but i get some error in the model
